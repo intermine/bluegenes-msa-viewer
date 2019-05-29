a56e6ff4f1b4091fa9a10daa692e18d3a724a55b
akshat <akshatbhargavadeepa@gmail.com>
2019-05-29 20:36:41 +0530

    chained queries, now receiving all sequences associated with the geneId passed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var msa = require('msa');
 var queryGeneToProtein = require('./queries/geneToProtein');
+var queryProteinToSeq = require('./queries/proteinToSequence');
 
 // make sure to export main, with the signature
 function main(el, service, imEntity, state, config) {
@@ -8,7 +9,23 @@ function main(el, service, imEntity, state, config) {
 		throw new Error('Call main with correct signature');
 	}
 
-	queryGeneToProtein(imEntity.value, service.root);
+	queryGeneToProtein(imEntity.value, service.root).then(res => {
+		var gene = res[0];
+		var proteins = gene.proteins;
+		var queries = [];
+		proteins.forEach(protein => {
+			var query = queryProteinToSeq(protein.primaryAccession, service.root);
+			queries.push(query);
+		});
+
+		// var fasta = '';
+		Promise.all(queries).then(results => {
+			results.forEach(() => {
+				// var sequence = result[0].sequence.residues;
+				// console.log(sequence);
+			});
+		});
+	});
 
 	var fasta = '>seq1\n\
 	ACTG\n\
