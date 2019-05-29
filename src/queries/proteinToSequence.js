e6a13156ae1902a8b6aea6ea5972dd80a7c56293
akshat <akshatbhargavadeepa@gmail.com>
2019-05-29 20:34:49 +0530

    added proteinToSequence query

---
+++ src/queries/proteinToSequence.js
@@ -0,0 +1,39 @@
+const queryProteinToSequence = primaryAccession => ({
+	name: 'Protein_Sequence',
+	title: 'Protein --> Sequence',
+	description:
+		'for a specified protein or list proteins give the protein sequence and length.',
+	from: 'Protein',
+	select: [
+		'primaryIdentifier',
+		'primaryAccession',
+		'sequence.length',
+		'sequence.residues'
+	],
+	orderBy: [
+		{
+			path: 'primaryIdentifier',
+			direction: 'ASC'
+		}
+	],
+	where: [
+		{
+			path: 'Protein.primaryAccession',
+			op: '=',
+			value: primaryAccession
+		}
+	]
+});
+
+// eslint-disable-next-line
+function queryData(primaryAccession, serviceUrl, imjsClient = imjs) {
+	return new Promise((resolve, reject) => {
+		const service = new imjsClient.Service({ root: serviceUrl });
+		service
+			.records(queryProteinToSequence(primaryAccession))
+			.then(resolve)
+			.catch(reject);
+	});
+}
+
+module.exports = queryData;
