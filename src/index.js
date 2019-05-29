5225aea62be565e58abea091cde2aa8b27b2c231
akshat <akshatbhargavadeepa@gmail.com>
2019-05-29 19:57:32 +0530

    added MSA viewer as dependency

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
-// add any imports if needed, or write your script directly in this file.
-// const SomePackage = require('PackageName');
+var msa = require('msa');
 
 // make sure to export main, with the signature
 function main(el, service, imEntity, state, config) {
-  if (!state) state = {};
+	if (!state) state = {};
 	if (!el || !service || !imEntity || !state || !config) {
 		throw new Error('Call main with correct signature');
 	}
-	// sample code here to convert the provided intermine object (e.g. perhaps
-	// an id) into an identifier the tool expects. e.g.:
-	// of course if your tool was built for intermine it might understand
-	// intermine ids already, but many others tools expect a gene symbol or
-	// protein accession, etc...
-	/**
-   * Example - you can delete this and replace with your own code *******
 
-    // protVista expects an accession, so convert intermine id to accession
+	var fasta = '>seq1\n\
+	ACTG\n\
+	>seq2\n\
+	ACGG\n';
 
-    var columnToConvert = config.columnMapping[imEntity.class][imEntity.format];
-    var accession = new imjs.Service(service)
-        .findById(imEntity.class, imEntity.value)
-        .then(function(response) {
-        //put some code here to initialise your tool.
-    });
+	var seqs = msa.io.fasta.parse(fasta);
 
-  */
-	el.innerHTML = `
-		<div class="rootContainer">
-			<h1>Your Data Viz Here</h1>
-		</div>
-	`;
+	var m = msa.default({
+		el: el,
+		seqs: seqs
+	});
+	m.render();
 }
 
 module.exports = { main };
