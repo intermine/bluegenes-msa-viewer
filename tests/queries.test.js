1318a2005181709f89253f9629faddc97681ceb8
akshat <akshatbhargavadeepa@gmail.com>
2019-05-31 19:18:00 +0530

    added tests for protein -> sequence

--- tests/queries.test.js
+++ tests/queries.test.js
@@ -1,5 +1,6 @@
 const imjs = require('imjs');
 const geneToProteinQuery = require('../src/queries/geneToProtein');
+const proteinToSeqQuery = require('../src/queries/proteinToSequence');
 
 describe('queries', () => {
 	describe('gene -> protein', () => {
@@ -39,7 +40,25 @@ describe('queries', () => {
 			return expect(queryRes).rejects.toBe('No associated proteins found!');
 		});
 	});
-	describe('gene -> protein', () => {
-		test('should return a promise resolving with gene having proteins', () => {});
+
+	describe('protein -> sequence', () => {
+		const mockData = {
+			proteinAccessionId: 'Q8IZ69',
+			serviceUrl: 'http://www.humanmine.org/human'
+		};
+		test('should return a promise resolving with sequence when passed correct protein accession id', () => {
+			const queryRes = proteinToSeqQuery(
+				mockData.proteinAccessionId,
+				mockData.serviceUrl,
+				imjs
+			);
+
+			expect(queryRes).toBeInstanceOf(Promise);
+			return queryRes.then(res => {
+				expect(res).toHaveProperty('sequence');
+				expect(res.sequence).toHaveProperty('residues');
+				expect(res.sequence.residues).toBeTruthy();
+			});
+		});
 	});
 });
